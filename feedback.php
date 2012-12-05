c107c6bb70a6aa0e6f24558b8c9c87eeb64acb75
Raman Buttar <raman.buttar@msn.com>
2012-12-05 14:34:01 -0800

    Committing final completed project

---
+++ feedback.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST["name"];
+$comments = $_POST["comments"];
+
+$to = "sfussa@gmail.com";
+//$to = "raman.buttar@gmail.com";
+$subject = "Nihaal 2010 - Feedback";
+$message = "Name: " . $name . "\n";
+$message .= "Comments: " . $comments;
+$headers = "From: Nihaal 2010 <website@nihaal.org>"; 
+
+if (((strlen($name)) > 0) && ((strlen($comments)) > 0)) {
+	if (mail($to, $subject, $message, $headers)) {
+		header( 'Location: index.html' ) ;
+	} else {
+		print "Error in sending feedback. Mail could not be sent.";
+	}
+} else {
+		print "Error in sending feedback. Blank Name and Comment.";
+}
+?>
